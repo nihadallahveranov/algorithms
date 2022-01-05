f8f1a81256de96eff8ae5ee2198713ba3f11620e
Nihad Allahveranov <96189108+nihadd99@users.noreply.github.com>
2022-01-06 01:15:44 +0400

    Create eolymp-2166.cpp

---
+++ eolymp-2166.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cmath>
+int main(){
+    std::string str1,str2;
+    std::cin >> str1 >> str2;
+    int size1=str1.length(),size2=str2.length(),count1=0,count2=0;
+    for ( short i = 0 ; i < size1 ; i++){
+        for ( short j = 0 ; j < size1 ; j++){
+            if (str1[i]==str1[j]){
+                count1++;
+            }
+        }
+        for ( short j = 0 ; j < size2 ; j++){
+            if (str1[i]==str2[j]){
+                count2++;
+            }
+        }
+        if (count2==0){
+            std::cout << "NO";
+            return 0;
+        }
+        if (count2<count1){
+            std::cout << "NO";
+            return 0;
+        }
+        count1=0;
+        count2=0;
+    }
+    std::cout << "YES";
+    return 0;
+}
