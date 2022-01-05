d447a76698c9ab75e20b84b5dd2cf99873847487
Nihad Allahveranov <96189108+nihadd99@users.noreply.github.com>
2022-01-06 01:36:09 +0400

    Create unique.sh

---
+++ unique.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+function unique_elements(){
+	local var_arr=( "$@" )    # create new local arr variable
+	local var_size=${#var_arr[@]}   # create new local size variable, show the size of array
+	local j=0
+	echo -n "unique elements: "    # -n  not new line
+	for (( i = 0 ; i < var_size ; i++ ))
+	do
+		for (( j = 0 ; j < i ; j++ ))
+		do
+			if (( var_arr[i]==var_arr[j] ))
+			then
+				break
+			fi
+		done
+		if (( i==j ))
+		then
+			echo -n "${var_arr[i]} "
+		fi
+	done
+}
+echo -e "Enter the size: \c"     # first step
+read size                            # input size of array
+for (( i = 0 ; i < size ; i++ ))
+do
+  read arr[i]      # input elements of arrray
+done
+unique_elements "${arr[@]}"     # send arr parameter to our function unique_elements, let's go unique_elements
