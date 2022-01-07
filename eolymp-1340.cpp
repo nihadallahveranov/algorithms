cd0b9da1dfdfc10452b28b1fe49581645e74568f
Nihad Allahveranov <nihad.allahveranov@gmail.com>
2022-01-07 19:43:37 +0400

    Create eolymp-1340.cpp

---
+++ eolymp-1340.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cmath>
+int main(){
+    short n;
+    while (std::cin >> n){
+        if (n==0){
+            return 0;
+        }
+        for ( short i = 0 ; i < n ; i++){
+            for ( short j = 1 ; j < n-i ; j++){
+                std::cout << " ";
+            }
+            for ( short j = 0 ; j < i*2+1 ; j++){
+                std::cout << "*";
+            }
+            std::cout << std::endl;
+        }
+        for ( short i = 1 ; i < n ; i++){
+            for ( short j = 0 ; j < i ; j++){
+                std::cout << " ";
+            }
+            for ( short j = 0 ; j < n*2-i*2-1 ; j++){
+                std::cout << "*";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+}
