e63924aefbb4af084684c7eee76c3b04d80c597c
Nihad Allahveranov <nihad.allahveranov@gmail.com>
2022-01-18 02:48:18 +0400

    Update eolymp-930.c

--- eolymp-930.c
+++ eolymp-930.c
@@ -7,28 +7,28 @@ int main(){
     short size = strlen(str), num = 0, num1 = 0, i = 0, j = 0;
     for ( ; i < 10 ; i++){
         for ( j = 0 ; j < size ; j++){
-            if (str[j]==i+48){
+            if (str[j] == i+48){
                 num++;
                 break;
             }
         }
-        if (num==0){
+        if (num == 0){
             num1++;
         }
-        num=0;
+        num = 0;
     }
     printf("%hd\n", num1);
-    num=0;
+    num = 0;
     for ( i = 0 ; i < 10 ; i++){
         for ( j = 0 ; j < size ; j++){
-            if (str[j]==i+48){
+            if (str[j] == i+48){
                 num++;
                 break;
             }
         }
-        if (num==0){
+        if (num == 0){
             printf("%hd ", i);
         }
-        num=0;
+        num = 0;
     }
 }
