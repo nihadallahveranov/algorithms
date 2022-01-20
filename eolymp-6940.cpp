5b6a39235ac803d3871e6491e5aa0f71c2029753
Nihad Allahveranov <nihad.allahveranov@gmail.com>
2022-01-21 01:59:21 +0400

    Update eolymp-6940.cpp

--- eolymp-6940.cpp
+++ eolymp-6940.cpp
@@ -4,16 +4,16 @@
 #include <cmath>
 void max_frequency(std::string* ptr, short n){
     short max = 0, count = 0;
-    for ( short i = 0 ; i < n ; i++ ){
-        for ( short j = 0 ; j < n ; j++ ){
+    for ( short i = n -1  ; i >= 0 ; i-- ){
+        for ( short j = n - 1 ; j >= 0 ; j-- ){
             ( ptr[i] == ptr[j] ) ? count++ : count = count;
         }
         ( max < count ) ? max = count : max = max;
         count = 0;
     }
     char max_ascii_letter = 0;
-    for ( short i = 0 ; i < n ; i++ ){
-        for ( short j = 0 ; j < n ; j++ ){
+    for ( short i = n -1  ; i >= 0 ; i-- ){
+        for ( short j = n - 1 ; j >= 0 ; j-- ){
             ( ptr[i] == ptr[j] ) ? count++ : count = count;
         }
         if ( max == count){
