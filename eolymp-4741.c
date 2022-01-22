5660c28152569453ac4b6024cbca2118842e0866
Nihad Allahveranov <nihad.allahveranov@gmail.com>
2022-01-22 21:54:19 +0400

    Update eolymp-4741.c

--- eolymp-4741.c
+++ eolymp-4741.c
@@ -4,12 +4,12 @@
 int bubblesort(int* ptr, short n){
     int num = 0;
     short i = 0, j = 1;
-    for ( ; i < n ; i++){
-        for ( j = 1 ; j < n-i ; j++){
-            if (ptr[j-1]>ptr[j]){
-                int temp=ptr[j-1];
-                ptr[j-1]=ptr[j];
-                ptr[j]=temp;
+    for ( ; i < n ; i++ ){
+        for ( j = 1 ; j < n-i ; j++ ){
+            if ( ptr[j-1] > ptr[j] ){
+                int temp = ptr[j-1];
+                ptr[j-1] = ptr[j];
+                ptr[j] = temp;
                 num++;
             }
         }
